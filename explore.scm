b06554b54156c6fbc208857098f76f587c7d2bb9
Jacob Reckhard <jacobreckhard@gmail.com>
2021-10-19 11:22:41 -0600

    Added travelling

--- explore.scm
+++ explore.scm
@@ -5,10 +5,21 @@
   #:use-module (lispymon utils)
   #:use-module (lispymon lispymon)
   #:use-module (srfi srfi-1)
+  #:use-module (ice-9 ftw)
+  #:use-module (ice-9 string-fun)
   #:export  (explore-display
              explore-update))
 
 (define area-path "assets/areas/")
+
+;;;;
+;;; Utilities
+;;;;
+
+(define (state-confirm data msg)
+  (alist-merge data `((state-data . ((next . ,(assoc-get 'state-data data))
+                                     (type . confirm)
+                                     (msg . ,msg))))))
 
 ;; Gets a random value from a weighted vector
 ;; Example:
@@ -27,28 +38,47 @@
       (loop cs-n (+ 1 idx))))
   (loop 0 0))
 
+(define (get-loc-string loc-name)
+  (load-json-file (string-append area-path loc-name)))
+(define (get-loc loc-name)
+  (get-loc-string (string-append (symbol->string loc-name) ".json")))
 (define (get-loc-from-data data)
   (define loc-name (assoc-get 'state-data data))
-  (load-json-file (string-append area-path (symbol->string loc-name) ".json")))
+  (get-loc loc-name))
 
+(define (area-symbol-from-name name)
+  (define files
+    (filter (lambda (x) (not (or (string=? ".." x) (string=? "." x))))
+            (scandir area-path)))
+  (define match
+    (filter (lambda (x)
+              (define xname (assoc-get "name" (get-loc-string x)))
+              (string=? (string-downcase xname) (string-downcase name)))
+            files))
+  (string->symbol
+    (string-replace-substring (car match) ".json" "")))
+
+(define (area-symbols-to-names areas)
+  (println "Blip" areas)
+  (map (lambda (x) (assoc-get "name" (get-loc-string (string-append x ".json"))))
+       areas))
+;;;;
+;;; Explore encounters
+;;;;
 (define (explore-encounter-dialog data loc)
   (define msg (random:vector (assoc-get '("explore" "dialog") loc)))
   (state-confirm data msg))
 
+;;;;
+;;; Top level options
+;;;;
 (define (explore-explore data)
   (define loc (get-loc-from-data data))
   (define encounters (assoc-get '("explore" "encounters") loc))
   (define encounter (random:weighted-vector encounters))
   (cond
    ((string=? "dialog" encounter) (explore-encounter-dialog data loc)))
-  ;; TODO: implement
-  (println "explore" encounter)
   data)
-
-(define (state-confirm data msg)
-  (alist-merge data `((state-data . ((next . ,(assoc-get 'state-data data))
-                                     (type . confirm)
-                                     (msg . ,msg))))))
 
 (define (explore-rest data)
   (define loc (get-loc-from-data data))
@@ -57,22 +87,40 @@
   (define success (>= rest-success (random:uniform)))
   (if success
       (let ((msg (random:vector (assoc-get "successes" rest-info))))
-        (println "here:" data)
         (state-confirm data msg))
           ;; TODO: actually heal something
       (let ((encounter "TODO"))
          data)))
 
+(define (explore-travel data)
+  (define loc (get-loc-from-data data))
+  (define travel-args (assoc-get '("travel" "locations") loc))
+  (define dests (append (area-symbols-to-names (vector->list travel-args)) (list "Cancel")))
+  (alist-merge data `((state-data . ((from . ,(assoc-get 'state-data data))
+                                     (type . travel)
+                                     (options . ,(list->vector dests)))))))
+(define (explore-travel2 data selection)
+  (define statedata (assoc-get 'state-data data))
+  (if (string=? "cancel" selection)
+    (alist-merge data `((state-data . ,(assoc-get 'from statedata))))
+    (alist-merge data `((state-data . ,(area-symbol-from-name selection))))))
+;;;;
+;;; Explore
+;;;;
 (define (explore-update data selection)
   (define statedata (assoc-get 'state-data data))
   (cond
    ((and (list? statedata) (eq? 'confirm (assoc-get 'type statedata)))
     (alist-merge data `((state-data . ,(assoc-get 'next statedata)))))
+   ((and (list? statedata) (eq? 'travel (assoc-get 'type statedata)))
+    (explore-travel2 data selection))
    ((symbol? statedata)
     (cond
       ((string=? selection "explore") (explore-explore data))
       ((string=? selection "rest") (explore-rest data))
-      (else (println "Invalid [explore] selection: " selection) data)))))
+      ((string=? selection "travel") (explore-travel data))
+      (else (println "Invalid [explore] selection: " selection) data)))
+   (else (println "Invalid [explore] selection2: " selection) data)))
 
 (define (explore-display statedata)
 
@@ -85,4 +133,9 @@
    ((and (list? statedata) (eq? 'confirm (assoc-get 'type statedata)))
     (values (string-append (assoc-get 'msg statedata))
             #("Okay")))
+   ((and (list? statedata) (eq? 'travel (assoc-get 'type statedata)))
+    (values (string-append "Travelling from "
+                           (car (area-symbols-to-names (list (symbol->string (assoc-get 'from statedata)))))
+                           ". Where would you like to go?")
+            (assoc-get 'options statedata)))
    (else (print "Unknown statedata:") (writeln statedata))))
